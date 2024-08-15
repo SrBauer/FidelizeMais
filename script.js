e584dbbea81af37f3b75df8707cbbdf80eb84ade
SrBauer <bauer.jonathan.jj@gmail.com>
2024-08-15 20:34:28 -0300

    inicio da criação da tela de cadastro

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-btn-experimente.addEventListener('click', ()=>{
-    
-})
+document.addEventListener('DOMContentLoaded', function () {
+    const btnExperimente = document.querySelector('.btn-experimente');
+    const modalCadastro = document.getElementById('modal-cadastro');
+
+    btnExperimente.addEventListener('click', function () {
+        modalCadastro.style.display = 'flex';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target == modalCadastro) {
+            modalCadastro.style.display = 'none';
+        }
+    });
+});
