1d074470366f7dc8619ed7c257b976893e9a1356
SrBauer <bauer.jonathan.jj@gmail.com>
2024-08-23 21:09:29 -0300

    Feat: caminho JS

--- script.js
+++ script.js
@@ -3,7 +3,25 @@ document.addEventListener('DOMContentLoaded', function () {
     const loginContainer = document.getElementById('login-container');
 
     btnEntrar.addEventListener('click', function (event) {
-        event.preventDefault(); // Evita o comportamento padrão do link
-        loginContainer.style.display = 'flex';
+        event.preventDefault(); // Evita o comportamento padrão do botão
+
+        // Captura os valores dos campos de entrada
+        const username = document.getElementById('username').value;
+        const company = document.getElementById('company').value;
+        const password = document.getElementById('password').value;
+
+        // Define os valores esperados (alterar conforme necessário)
+        const expectedUsername = "usuarioExemplo";
+        const expectedCompany = "empresaExemplo";
+        const expectedPassword = "senhaExemplo";
+
+        // Valida os dados de login
+        if (username === expectedUsername && company === expectedCompany && password === expectedPassword) {
+            // Redireciona para a página index.html
+            <script>window.location.href = 'index.html';</script>
+        } else {
+            // Exibe uma mensagem de erro
+            alert("Dados de login inválidos. Por favor, tente novamente.");
+        }
     });
 });
